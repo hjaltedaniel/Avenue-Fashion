4c6102ecfa168404f9de1e690414b142cedfbf0e
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-02-27 19:06:19 +0100

    Creating routes for new views

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from './views/HomePage.vue'
 import ProductPage from './views/ProductPage.vue'
+import GenderCollection from './views/GenderCollection.vue'
+import CategoryCollection from './views/CategoryCollection.vue'
 import ContentPage from './views/ContentPage.vue'
 import UserPage from './views/UserPage.vue'
 
@@ -19,6 +21,18 @@ export default new Router({
       path: '/products/:id',
       name: 'product',
       component: ProductPage,
+      props: true
+    },
+    {
+      path: '/gender/:gender',
+      name: 'gender',
+      component: GenderCollection,
+      props: true
+    },
+    {
+      path: '/category/:gender/:style/:category',
+      name: 'category',
+      component: CategoryCollection,
       props: true
     },
     {
