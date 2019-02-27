4b74d015d04368b9b2a2ea4726f070cf3ffaa697
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-02-27 23:02:27 +0100

    Creating content on contentpage

--- src/views/ContentPage.vue
+++ src/views/ContentPage.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    <h1>This is contentpage {{ id }}</h1>
+  <div v-if="content != 0">
+    <h1>{{ content.name }}</h1>
+    <h2>{{ content.byline }}</h2>
+    <img v-bind:src="'@/assets/images/content/' + content.section1.picture">
+  </div>
+  <div v-else>
+    <h1>404
+      <br>
+      No contentpage with id: {{ id }} is found!
+    </h1>
   </div>
 </template>
 
 <script>
+import ApiService from "@/services/ApiService.js";
 export default {
-  props: ["id"]
+  props: ["id"],
+  data() {
+    return {
+      content: {}
+    };
+  },
+  created() {
+    ApiService.getContent(this.id)
+      .then(response => {
+        this.content = response.data;
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response);
+      });
+  }
 };
 </script>
 
