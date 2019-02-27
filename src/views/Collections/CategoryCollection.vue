9ad1986be6421950783ff0d1f2f9457f5091fd8e
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-02-27 23:00:19 +0100

    Moving collections

---
+++ src/views/Collections/CategoryCollection.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="products != 0">
+    <ul v-for="product in products" :key="product.id">
+      <router-link :to="{ name: 'product', params: { id: product.id } }">
+        <li>{{ product.name }}</li>
+      </router-link>
+    </ul>
+  </div>
+  <div v-else>
+    <h1>No products of category {{ cat.name }} available</h1>
+  </div>
+</template>
+
+<script>
+import ApiService from "@/services/ApiService.js";
+export default {
+  props: ["gender", "style", "category"],
+  data() {
+    return {
+      products: [],
+      cat: {}
+    };
+  },
+  created() {
+    ApiService.getCategoryProducts(this.gender, this.style, this.category)
+      .then(response => {
+        this.products = response.data;
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response);
+      });
+    ApiService.getCategory(this.category)
+      .then(response => {
+        this.cat = response.data;
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response);
+      });
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/views/Collections/StyleCollection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="products != 0">
+    <ul v-for="product in products" :key="product.id">
+      <router-link :to="{ name: 'product', params: { id: product.id } }">
+        <li>{{ product.name }}</li>
+      </router-link>
+    </ul>
+  </div>
+  <div v-else>
+    <h1>No products of style {{ style }} available</h1>
+  </div>
+</template>
+
+<script>
+import ApiService from "@/services/ApiService.js";
+export default {
+  props: ["gender", "style"],
+  data() {
+    return {
+      products: []
+    };
+  },
+  created() {
+    ApiService.getStyleProducts(this.gender, this.style)
+      .then(response => {
+        this.products = response.data;
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response);
+      });
+  }
+};
+</script>
+
+<style scoped>
+</style>
