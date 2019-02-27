92f879127864b48e937edbe7c70859f7f3fa3277
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-02-27 19:56:31 +0100

    Setting up category pages to take data about the active category

--- src/views/CategoryCollection.vue
+++ src/views/CategoryCollection.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
+  <div v-if="products != 0">
     <ul v-for="product in products" :key="product.id">
       <router-link :to="{ name: 'product', params: { id: product.id } }">
         <li>{{ product.name }}</li>
       </router-link>
     </ul>
+  </div>
+  <div v-else>
+    <h1>No products of category {{ cat.name }} available</h1>
   </div>
 </template>
 
@@ -14,7 +17,8 @@ export default {
   props: ["gender", "style", "category"],
   data() {
     return {
-      products: []
+      products: [],
+      cat: {}
     };
   },
   created() {
@@ -25,9 +29,21 @@ export default {
       .catch(error => {
         console.log("There was an error:" + error.response);
       });
+    ApiService.getCategories("mens")
+      .then(response => {
+        let y = [];
+        let category = this.category;
+        y = response.data;
+        let x = y.filter(function(cat) {
+          return cat.id == category;
+        });
+        this.cat = x[0];
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response);
+      });
   }
 };
 </script>
-
 <style scoped>
 </style>
