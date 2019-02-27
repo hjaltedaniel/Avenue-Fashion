dd6370f8f812eba7033c19c1d42483e136eb0365
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-02-27 19:06:45 +0100

    Introducing new API methods to handle new data

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -13,7 +13,16 @@ export default {
   getProducts() {
     return apiClient.get("/products")
   },
+  getGenderProducts(gender) {
+    return apiClient.get('/products?gender=' + gender)
+  },
+  getCategoryProducts(gender, style, category) {
+    return apiClient.get('/products?gender=' + gender + "&style=" + style + "&category=" + category)
+  },
+  getCategories(gender) {
+    return apiClient.get('/productCategories?gender=' + gender)
+  },
   getProduct(id) {
-    return apiClient.get('products/' + id)
+    return apiClient.get('/products/' + id)
   }
 }
