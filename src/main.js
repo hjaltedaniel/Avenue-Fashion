b006064a600679d1351203180a6e0a25d94fe4f5
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-03-03 21:50:27 +0100

    Adding pwa stuff

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store';
 import VeeValidate from 'vee-validate';
+import './registerServiceWorker'
 
 Vue.use(VeeValidate);
 
