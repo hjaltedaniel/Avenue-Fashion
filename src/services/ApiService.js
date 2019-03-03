e80a7643189f563d1a29e8be4c379ce7164ee30b
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-03-03 20:34:49 +0100

    Updating to remote api

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `http://167.99.41.112:8080`,
   withCredentials: false,
   headers: {
     Accept: "application/json",
